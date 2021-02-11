916e7f9ace4974839a82974b267d207b3551ba20
Fernando Pena <nando.pena@gmail.com>
2021-02-11 15:59:02 -0300

    Aula 04

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
 import theme from '../src/theme';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`;
+import { GlobalStyle } from '../src/theme/GlobalStyle';
 
 // eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+      <Head>
+        <title>Instalura - Pena</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </ThemeProvider>
--- src/theme/index.js
+++ src/theme/index.js
@@ -42,5 +42,6 @@ const colors = {
 export default {
   colors,
   borderRadius: '12px',
+  fontFamily: '\'Rubik\', sans-serif',
   transition: '200ms ease-in-out',
 };

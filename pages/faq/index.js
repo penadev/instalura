04884edaef25be114786e8ecaf31be00e23daf68
Fernando Pena <nando.pena@gmail.com>
2021-03-14 22:23:31 -0300

    perf(arquiteture): change arquiteture to get dynamic pages

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import FAQScreen from '../../src/components/screens/FAQScreen';
+import websitePageHOC from '../../src/components/wrappers/WebsitePage/hoc';
 
-export default function FAQPage(props) {
+function FAQPage({ faqCategories }) {
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
-    <FAQScreen {...props} />
+    <FAQScreen faqCategories={faqCategories} />
   );
 }
 
+FAQPage.propTypes = FAQScreen.propTypes;
+
+export default websitePageHOC(FAQPage, {
+  pageWrapperProps: {
+    seoProps: {
+      headTitle: 'Perguntas Frequentes',
+    },
+  },
+});
+
 export async function getStaticProps() {
-  const faqCategories = await fetch('https://instalura-api.vercel.app/api/content/faq').then(async (res) => {
-    const response = await res.json();
-    return response.data;
-  });
+  const faqCategories = await fetch('https://instalura-api.vercel.app/api/content/faq')
+    .then((respostaDoServer) => respostaDoServer.json())
+    .then((respostaConvertida) => respostaConvertida.data);
 
   return {
     props: {
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,20 @@
 import React from 'react';
-import Menu from '../src/components/commons/Menu';
-import Footer from '../src/components/commons/Footer';
 import Text from '../src/components/foundation/Text';
 import { Button } from '../src/components/commons/Button';
 import { Grid } from '../src/components/foundation/layout/Grid';
+import { WebsitePageContext } from '../src/components/wrappers/WebsitePage';
 import { Box } from '../src/components/foundation/layout/Box';
-import Modal from '../src/components/commons/Modal';
-import FormCadastro from '../src/components/patterns/FormCadastro';
-import SEO from '../src/components/commons/SEO';
+import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
 
-export default function Home() {
-  const [isModalOpen, setModalState] = React.useState(false);
+function HomeScreen() {
+  const websitePageContext = React.useContext(WebsitePageContext);
 
   return (
     <Box
       flex="1"
       display="flex"
-      flexWrap="wrap"
       flexDirection="column"
-      justifyContent="space-between"
-      backgroundImage="url(/images/bubbles.svg)"
-      backgroundRepeat="no-repeat"
-      backgroundPosition="bottom right"
     >
-      <SEO headTitle="Home" />
-
-      <Modal
-        isOpen={isModalOpen}
-        onClose={() => {
-          setModalState(false);
-        }}
-      >
-        {(propsDoModal) => (
-          <FormCadastro propsDoModal={propsDoModal} />
-        )}
-      </Modal>
-
-      <Menu
-        onCadastrarClick={() => setModalState(true)}
-      />
-
       <Grid.Container
         marginTop={{
           xs: '32px',
@@ -59,7 +34,6 @@ export default function Home() {
               variant="title"
               tag="h1"
               color="tertiary.main"
-              // textAlign="right"
               textAlign={{
                 xs: 'center',
                 md: 'left',
@@ -87,9 +61,7 @@ export default function Home() {
               }}
               display="block"
               variant="primary.main"
-              onClick={() => {
-                setModalState(!isModalOpen); // novo state sendo atribuido
-              }}
+              onClick={() => websitePageContext.toggleModalCadastro()}
             >
               Cadastrar
             </Button>
@@ -108,8 +80,19 @@ export default function Home() {
           </Grid.Col>
         </Grid.Row>
       </Grid.Container>
-
-      <Footer />
     </Box>
   );
 }
+
+export default websitePageHOC(HomeScreen, {
+  pageWrapperProps: {
+    seoProps: {
+      headTitle: 'Home',
+    },
+    pageBoxProps: {
+      backgroundImage: 'url(/images/bubbles.svg)',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'bottom right',
+    },
+  },
+});
